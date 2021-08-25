2c5da9663ea88270341120421caf63a4b11f2dc5
T-Damer <dpancov64@gmail.com>
2021-08-25 22:52:15 +0300

    Added media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,7 +72,6 @@ window.addEventListener('scroll', scrollActive)
 
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 200)
     scrollTop.classList.add('show-scroll')
   else scrollTop.classList.remove('show-scroll')
