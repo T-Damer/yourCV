0c21a7cc4d5d30c8e8f2103f4cf43c32ea262e10
T-Damer <dpancov64@gmail.com>
2021-08-25 23:00:29 +0300

    Made dark-light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,6 @@ const getCurrentIcon = () =>
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[
     selectedTheme === 'dark' ? 'add' : 'remove'
   ](darkTheme)
